637ae9d4672d593c773cea4527f106c8d76c427b
chrissantillan <105517892+chrissantillan@users.noreply.github.com>
2023-10-11 15:58:59 -0500

    Removed old Harvard BLTI stuff from create_topic.php

--- create_topic.php
+++ create_topic.php
@@ -17,9 +17,7 @@
  * @link     http://unlockedlabs.org
  */
 
- namespace unlockedlabs\unlocked;
-
-use LTITool\Tool;
+namespace unlockedlabs\unlocked;
 
 require_once 'session-validation.php';
 //ensure admin user (admin is 2 and above)
@@ -33,82 +31,6 @@ require_once dirname(__FILE__).'/config/database.php';
 require_once dirname(__FILE__).'/objects/topic.php';
 require_once dirname(__FILE__).'/objects/users.php';
 require_once dirname(__FILE__).'/objects/category.php';
-
-// include LTI library and utility files
-require_once dirname(__FILE__).'/LTI/misc.php';
-require_once dirname(__FILE__).'/LTI/ims-blti/blti_util.php';
-require_once dirname(__FILE__).'/LTI/Tool/config/database.php';
-require_once dirname(__FILE__).'/LTI/objects/tool.php';
-
-
-// This is a dummy comment for Jira testing
-$database2 = new \LTITool\Database();
-// $database2 = new \LTITool\Database();
-$db2 = $database2->getConnection();
-$tool = new \LTITool\Tool($db2);
-
-// dummy data for LTI Launch
-$lmsdata = array(
-    "resource_link_id" => "120988f929-274612",
-    "resource_link_title" => "Weekly Blog",
-    "resource_link_description" => "A weekly blog.",
-    "user_id" => "292832126",
-    "roles" => "Instructor",  // or Learner
-    "lis_person_name_full" => 'Jane Q. Public',
-    "lis_person_name_family" => 'Public',
-    "lis_person_name_given" => 'Given',
-    "lis_person_contact_email_primary" => "user@school.edu",
-    "lis_person_sourcedid" => "school.edu:user",
-    "context_id" => "456434513",
-    "context_title" => "Design of Personal Environments",
-    "context_label" => "SI182",
-    "tool_consumer_info_product_family_code" => "ims",
-    "tool_consumer_info_version" => "1.1",
-    "tool_consumer_instance_guid" => "lmsng.school.edu",
-    "tool_consumer_instance_description" => "University of School (LMSng)",
-);
-
-if ($_POST) {
-    // for the POST back to replace dummy values with POST values
-    foreach ($lmsdata as $k => $val ) {
-        if (isset($_POST[$k])) {
-          if ( $_POST[$k] && strlen($_POST[$k]) > 0 ) {
-              $lmsdata[$k] = $_POST[$k];
-          }
-        }
-    }
-}
-
-
-$cur_url = curPageURL();
-
-// A consumer key can be any unique string; it could, for example be the domain
-// name of the Tool Consumer system, or simply a GUID
-$ltikey = @$_REQUEST["ltikey"];
-// $ltikey ? $ltikey : $ltikey = "12345";
-$ltikey = ($ltikey ? $ltikey : "12345");
-
-// A shared secret is used to secure the messages sent between the systems, it should,
-// therefore, be strong - a random string of at least 15 characters, some systems use
-// a GUID for the secret
-$secret = @$_REQUEST["secret"];
-$secret = ($secret ? $secret : "secret");
-
-$endpoint = @$_REQUEST["endpoint"];
-
-$b64 = base64_encode($ltikey.":::".$secret);
-
-$endpoint = ($endpoint ? $endpoint : str_replace("create_topic.php","LTI/tool.php",$cur_url));
-
-$outcomes = @$_REQUEST["outcomes"];
-$outcomes = ($outcomes ? $outcomes : str_replace("create_topic.php","tool_consumer_outcome.php",$cur_url) . "?b64=" . htmlentities($b64));
-
-$tool_consumer_instance_guid = $lmsdata['tool_consumer_instance_guid'];
-$tool_consumer_instance_description = $lmsdata['tool_consumer_instance_description'];
-
-$xmldesc = @$_REQUEST["xmldesc"];
-$xmldesc = ($xmldesc ? str_replace("\\\"","\"",$_REQUEST["xmldesc"]) : $default_desc);
-
 
 // instantiate database and product object
 $database = new Database();
@@ -177,69 +99,6 @@ if ($_POST) {
     </div>
 </div>
 
-<div class="card container">
-    <div class="card-body">
-        <h3 class="card-title text-center"><?php echo $category_name; ?></h3>
-        <h3 class="card-title">Create New LTI 1.1 Consumer Launch</h3>
-        <h4>This is a very simple reference implementation of the LMS side (i.e. consumer) for IMS LTI 1.1.</h4>
-        <a id="displayText" href="">Toggle Resource and Launch Data</a>
-        <!-- <a id="displayText" href="javascript:lmsdataToggle();">Toggle Resource and Launch Data</a> -->
-    <div id="lmsDataForm" style="display:block">
-        <form method="post">
-            <input type="submit" value="Recompute Launch Data">
-            <input type="reset" name="reset" value="Reset">
-            <fieldset>
-                <legend>Basic LTI Resource</legend>
-                <div class="form-group">
-                    <label for="launchUrl">Launch URL</label>
-                    <!-- <input type="text" name='endpoint' size="60" id="launchUrl"><br> -->
-                    <input type="text" name='endpoint' size="60" id="launchUrl" value=<?php echo $endpoint ?>><br>
-                    <label for="ltiKey">Key</label>
-                    <!-- <input type="text" name='ltikey' size="60" id="ltiKey"><br> -->
-                    <input type="text" name='ltikey' size="60" id="ltiKey" value=<?php echo $ltikey ?>><br>
-                    <label for="secret">Secret</label>
-                    <!-- <input type="text" name='secret' size="60" id="secret"> -->
-                    <input type="text" name='secret' size="60" id="secret" value=<?php echo $secret ?>>
-                </div>
-            </fieldset>
-<?php
-echo("<fieldset><legend>Launch Data</legend>\n");
-foreach ($lmsdata as $k => $val ) {
-    echo($k.": <input type=\"text\" name=\"".$k."\" value=\"");
-    echo(htmlspecialchars($val));
-    echo("\"><br/>\n");
-}
-echo("</fieldset>\n");
-echo("</form>\n");
-echo("</div>\n");
-// echo("<input type=\"submit\" id=\"ltiLaunch\" name=\"ext_submit\" value=\"Press to Launch\">\n");
-echo("<hr>");
-
-$parms = $lmsdata;
-// Cleanup parms before we sign
-foreach( $parms as $k => $val ) {
-    if (strlen(trim($parms[$k]) ) < 1 ) {
-        unset($parms[$k]);
-    }
-}
-
-// Add oauth_callback to be compliant with the 1.0A spec
-$parms["oauth_callback"] = "about:blank";
-if ( $outcomes ) {
-    $parms["lis_outcome_service_url"] = $outcomes;
-    $parms["lis_result_sourcedid"] = "feb-123-456-2929::28883";
-}
-
-$parms = signParameters($parms, $endpoint, "POST", $ltikey, $secret, "Press to Launch", $tool_consumer_instance_guid, $tool_consumer_instance_description);
-
-$content = postLaunchHTML($parms, $endpoint, true, "width=\"100%\" height=\"900\" scrolling=\"auto\" frameborder=\"1\" transparency");
-print($content);
-?>
-        </div>
-    </div>
-</div>
-
-
 <script>
 /* 
 Immediately invoked function (IIFE).
@@ -288,56 +147,7 @@ $('#create-topic-form').on('submit', function(e) {
 });
 }) ();
 
-(function(){
-
-$('#displayText').on('click', function(e) {
-
-    e.preventDefault();
-    console.log("I was clicked!");
-
-    var $content = $("#lmsDataForm");
-    if ($content.css("display") == "block") {
-        $content.css("display", "none");
-    }
-    else {
-        $content.css("display", "block");
-    }
-});
-
-$('#displayDebugData').on('click', function(e) {
-
-    e.preventDefault();
-
-    var $content = $("#basicltiDebug");
-    if ($content.css("display") == "block") {
-        $content.css("display", "none");
-    }
-    else {
-        $content.css("display", "block");
-    }
-});
-
-$('#ltiLaunch').on('click', function(e) {
-    $('#ltiLaunchForm').submit();
-});
-
-}) ();
 </script>
-
-<!-- just a simple toggle for the LMS Resource and Launch Data -->
-<!-- <script language="javascript"> 
-  //<![CDATA[ 
-function lmsdataToggle() {
-    var ele = document.getElementById("lmsDataForm");
-    if(ele.style.display == "block") {
-        ele.style.display = "none";
-    }
-    else {
-        ele.style.display = "block";
-    }
-} 
-  //]]> 
-</script> -->
 
 <?php
 // include page footer HTML
